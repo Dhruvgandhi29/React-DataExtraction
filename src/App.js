f8acf1f4d0b63725c4a24adeafa71afa3a7db521
Dhruv Gandhi <dhruvswidin@gmail.com>
2023-10-10 14:05:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Dataex from "./Components/Data";
 function App() {
+  const appstyle = { width: "100%", minHeight: "100vh", minWidth: "100vw" };
+  const dataexStyle = { width: "100%", height: "100%" };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={appstyle}
+      className="background d-flex flex-column justify-content-center align-items-center"
+    >
+      <h1 className="text-center text-light fw-bold">Dummy data</h1>
+      <div style={dataexStyle}>
+        <Dataex />
+      </div>
     </div>
   );
 }
